a597a3c2626cf38cab7784ccf3bfe451255fb97c
leonardo <bemmenos@gmail.com>
2023-12-05 13:08:47 -0300

    first

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    //
+    public function index()
+    {
+        $series = [
+            'Grey\'s Anatomy',
+            'Lost',
+            'Agents of SHIELD'
+        ];
+
+        $html = "<ul>";
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= "</ul>";
+
+        echo $html;
+    }
 }
